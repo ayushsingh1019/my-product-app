6eec3977943afef9f2288d53b952dbcc86df769a
ayushsingh1019 <ayush.r1019@gmail.com>
2025-01-18 03:43:20 +0530

    code change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <Router basename="/my-product-app">
+    <Router>
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/cart" element={<CartPage />} />
